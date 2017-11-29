1f83ae328c34b7adb36c8403c91a90f101f99794
jluzio <joao.luzio@gmail.com>
2017-11-29 14:56:02 +0000

    Added multipage support Challenges

---
+++ playground/app/src/main/java/org/example/jluzio/playground/Challenge20171128x01Activity.java
@@ -0,0 +1,13 @@
+package org.example.jluzio.playground;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Challenge20171128x01Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_challenge20171128x01);
+    }
+}
