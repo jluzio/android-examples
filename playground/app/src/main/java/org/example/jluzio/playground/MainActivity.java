e951f2238e4e56666852e14eccb33f53d82c3499
jluzio <joao.luzio@gmail.com>
2017-12-22 20:06:11 +0000

    Added multipage support

--- playground/app/src/main/java/org/example/jluzio/playground/MainActivity.java
+++ playground/app/src/main/java/org/example/jluzio/playground/MainActivity.java
@@ -1,9 +1,12 @@
 package org.example.jluzio.playground;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,6 +16,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -81,22 +86,67 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        Class<?> componentClass = null;
-        if (id == R.id.nav_sample) {
-            componentClass = SampleActivity.class;
-        } else if (id == R.id.nav_sample_by_screen_size) {
-            componentClass = SampleByScreenSizeActivity.class;
-        } else if (id == R.id.nav_challenge20171127x01) {
-            componentClass = Challenge20171127x01Activity.class;
-        } else if (id == R.id.nav_sample_by_screen_size) {
-            componentClass = Challenge20171128x01Activity.class;
-        }
-        if (componentClass != null) {
-            startActivity(new Intent(this, componentClass));
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        boolean isStartActivity = true;
+        if (isStartActivity) {
+            Class<?> targetViewClass = getActivityClassByNavigationItem(id);
+            if (targetViewClass != null) {
+                startActivity(new Intent(this, targetViewClass));
+            }
+        } else {
+            LayoutInflater inflater = getLayoutInflater();
+            ViewGroup container = findViewById(getLayoutByNavigationItem(id));
+            inflater.inflate(R.layout.activity_main, container);
+
+//            FrameLayout frameLayout = findViewById(R.id.content_main);
+
+//            FragmentManager fragmentManager = getFragmentManager();
+//            Fragment targetView = (Fragment) findViewById(getLayoutByNavigationItem(id));
+//            fragmentManager.beginTransaction().replace(R.id.content_main, targetView).commit();
+
+            //drawer.openDrawer(findViewById(id));
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    public Object getActivityByNavigationItem(int id) {
+        try {
+            Class<?> activityClass = getActivityClassByNavigationItem(id);
+            return activityClass != null ? activityClass.newInstance() : null;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Class<?> getActivityClassByNavigationItem(int id) {
+        Class<?> activityClass = null;
+        if (id == R.id.nav_sample) {
+            activityClass = SampleActivity.class;
+        } else if (id == R.id.nav_sample_by_screen_size) {
+            activityClass = SampleByScreenSizeActivity.class;
+        } else if (id == R.id.nav_challenge20171127x01) {
+            activityClass = Challenge20171127x01Activity.class;
+        } else if (id == R.id.nav_challenge20171128x01) {
+            activityClass = Challenge20171128x01Activity.class;
+        }
+        return activityClass;
+    }
+
+    public Integer getLayoutByNavigationItem(int id) {
+        Integer layout = null;
+        if (id == R.id.nav_sample) {
+            layout = R.layout.activity_sample;
+        } else if (id == R.id.nav_sample_by_screen_size) {
+            layout = R.layout.activity_sample_by_screen_size;
+        } else if (id == R.id.nav_challenge20171127x01) {
+            layout = R.layout.activity_challenge20171127x01;
+        } else if (id == R.id.nav_challenge20171128x01) {
+            layout = R.layout.activity_challenge20171128x01;
+        }
+        return layout;
+    }
+
 }
