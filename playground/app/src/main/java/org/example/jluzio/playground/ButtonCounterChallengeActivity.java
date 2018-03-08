1de73b70ff0847122eac59496bfc2183e01f4baa
João Luzio <joao.luzio@gmail.com>
2018-03-08 20:20:36 +0000

    Button Counter Challenge

--- playground/app/src/main/java/org/example/jluzio/playground/ButtonCounterChallengeActivity.java
+++ playground/app/src/main/java/org/example/jluzio/playground/ButtonCounterChallengeActivity.java
@@ -1,13 +1,41 @@
 package org.example.jluzio.playground;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class ButtonCounterChallengeActivity extends AppCompatActivity {
+    interface SavedStateKey {
+        public static final String TEXT_VIEW = "ButtonCounterChallengeActivity.textView";
+    }
+    private EditText userInput;
+    private Button submitButton;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_button_counter_challenge);
+
+        userInput = findViewById(R.id.inputText);
+        submitButton = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+
+        userInput.setText("");
+        textView.setText("");
+//        if (savedInstanceState != null) {
+//            textView.setText(savedInstanceState.getString(SavedStateKey.TEXT_VIEW, ""));
+//        }
+        textView.setMovementMethod(new ScrollingMovementMethod());
+
+        submitButton.setOnClickListener(view -> {
+            textView.append(String.format("%s%n", userInput.getText()));
+            userInput.setText("");
+        });
     }
+
+
 }
